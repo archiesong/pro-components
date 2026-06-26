e2b9329bd0ad03235b2d65df6653c7f0ba88c255
lucas <skyarchieqd@gmail.com>
2026-06-26 12:15:17 +0800

    docs(compiler): docs delete base config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -18,7 +18,6 @@ const docsBuildTarget = ['chrome111', 'edge111', 'firefox114', 'safari16.4', 'io
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pro-components/',
   plugins: [
     // virtualAntdCss({
     //   development: false,
